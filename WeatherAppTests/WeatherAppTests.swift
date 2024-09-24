043edb29a34c876bbb980b6ae0eea365045af3ea
Saideep9835 <tsaideep98@gmail.com>
2024-09-24 11:28:27 -0500

    Initial Commit

---
+++ WeatherAppTests/WeatherAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppTests.swift
+//  WeatherAppTests
+//
+//  Created by Saideep Reddy Talusani on 9/24/24.
+//
+
+import Testing
+@testable import WeatherApp
+
+struct WeatherAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
